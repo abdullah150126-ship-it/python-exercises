4375f689a380d812b0182d1f01fb6c0e6a98bb98
Abdullah Akram <abdullah150126@gmail.com>
2026-09-04 13:47:30 +0300

    change

--- mod1/program1.py
+++ mod1/program1.py
@@ -1,1 +1,11 @@
-print("Hello Abdullah Akram ")
+def change():
+    city = "Vantaa"
+    print("At the end of the function: " + city)
+    return
+
+city = "Helsinki"
+print("At the beginning in the main program: " + city)
+change()
+print("At the end of the main program: " + city)
+
+    
