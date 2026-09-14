f8ca753543c4bc15614083369ff200b871f962be
Abdullah Akram <abdullah150126@gmail.com>
2026-09-14 11:11:59 +0300

    mode 2 program2

--- mod1/program1.py
+++ mod1/program1.py
@@ -1,1 +1,8 @@
-print("Hello, Abdullah Akram")
+def sum_of_squares(first, second):
+    result = first**2 + second**2
+    return result
+
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: "))
+result = sum_of_squares(number1, number2)
+print(f"The sum of squares for numbers {number1:.3f} and {number2:.3f} is {result:.3f}.")
